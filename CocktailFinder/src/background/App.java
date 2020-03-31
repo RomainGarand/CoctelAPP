b076d4eb04160e424a3dde9c21331ef70c37fb15
Romain <romain.contrain@etu.enseeiht.fr>
2020-04-01 00:27:17 +0200

    Wrote some documentation and added calls to mockups

--- CocktailFinder/src/background/App.java
+++ CocktailFinder/src/background/App.java
@@ -1,9 +1,18 @@
 package background;
 
+import java.util.List;
+
+import services.CocktailSearcher;
+import services.DrinkIdentifier;
+import util.Photo;
+
 public class App {
 
 	public static void main(String[] args) {
 		//Create the UI Elements
+		
+		DrinkIdentifier di = new DrinkIdentifier();
+		CocktailSearcher cs = new CocktailSearcher();
 		
 		//Start the UI
 		
@@ -13,19 +22,22 @@ public class App {
 			
 			//Get the option selected by the user			
 			//Get a picture
+			Photo photo = new Photo();
 			
 			//Identify the drinks in the picture -> get a list of strings enumerating types of drinks
 				// IF the option selected is QR code
 					// call the QR code identifier
 				// ELSE (the option selected is bottles)
 					// call the drink identifier
+					List<String> ingredients =  di.identifyDrinks(photo);
 			
 			//Display the list of drinks in the UI
 			//Get corrections from the user
 			
 			//Search for recipes using the ingredients listed
 				//Call the coktail searcher
-			
+				cs.findRecipe(ingredients);
+					
 			//Display relevant information in the UI
 			
 			
--- CocktailFinder/src/services/CocktailSearcher.java
+++ CocktailFinder/src/services/CocktailSearcher.java
@@ -2,9 +2,16 @@ package services;
 
 import java.util.List;
 
+import util.Recipe;
+
 public class CocktailSearcher {
 	
-	public List<String> findRecipe(List<String> ingredients) {
+	/** Finds recipes in theCocktailDB that use given ingredients
+	 * 
+	 * @param ingredients a list of strings enumerating cocktail ingredients
+	 * @return a List of Recipe objects
+	 */
+	public List<Recipe> findRecipe(List<String> ingredients) {
 		
 	}
 
